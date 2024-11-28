02f33863044306334f299d47c318dc234d34cf05
IHNGEYMING <164995233+IHNGEYMING@users.noreply.github.com>
2024-11-28 08:07:05 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 sudo apt-get install aapt wget python3 python3-pip zipalign imagemagick openjdk-8-jdk -y
 wget https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/linux/apktool
-wget https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.8.1.jar
+wget https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.10.0.jar
 sudo mv apktool /usr/bin/apktool && sudo chmod +x /usr/bin/apktool
-sudo mv apktool_2.8.1.jar /usr/bin/apktool.jar && sudo chmod +x /usr/bin/apktool.jar
+sudo mv apktool_2.10.0.jar /usr/bin/apktool.jar && sudo chmod +x /usr/bin/apktool.jar
 pip install Pillow
